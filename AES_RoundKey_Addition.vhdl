ca53371d252836949930f6598e0fe4aa0c30b549
adityacollective <163301086+adityacollective@users.noreply.github.com>
2024-03-13 17:36:00 +0530

    Add files via upload

---
+++ AES_RoundKey_Addition.vhdl
@@ -0,0 +1,16 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+
+ENTITY RoundkeyAddition IS
+		PORT (RoundkeyAddition_in, RoundKey : IN std_logic_vector (127 downto 0);
+			 RoundkeyAddition_out : OUT std_logic_vector (127 downto 0));
+END RoundkeyAddition;
+
+
+ARCHITECTURE RoundkeyAddition_logic OF RoundkeyAddition IS
+BEGIN
+
+		RoundkeyAddition_out <= RoundkeyAddition_in XOR RoundKey;
+
+END roundkeyaddition_logic;
